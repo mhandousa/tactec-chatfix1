86ac97dc57ff17bc6456057545f2ca3bfe667649
mhandousa <53653174+mhandousa@users.noreply.github.com>
2025-09-17 21:31:26 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,70 @@
+"use client";
+
+import { Link } from "react-scroll";
+
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 bg-black/70 backdrop-blur border-b border-white/10">
       <div className="max-w-6xl mx-auto flex items-center justify-between p-4">
-        <img src="/images/tactec-2nd-lockup.png" alt="TacTec" className="h-10" />
+        {/* Logo */}
+        <img
+          src="/images/tactec-2nd-lockup.png"
+          alt="TacTec"
+          className="h-10"
+        />
+
+        {/* Navigation */}
         <nav className="flex gap-6 text-gray-300">
-          <a href="#challenge" className="hover:text-white">Challenge</a>
-          <a href="#solution" className="hover:text-white">Solution</a>
-          <a href="#features" className="hover:text-white">Features</a>
-          <a href="#tech" className="hover:text-white">Tech</a>
-          <a href="#start" className="hover:text-white">Get Started</a>
+          <Link
+            to="challenge"
+            smooth={true}
+            spy={true}
+            offset={-80}
+            activeClass="text-white font-bold"
+            className="cursor-pointer hover:text-white"
+          >
+            Challenge
+          </Link>
+          <Link
+            to="solution"
+            smooth={true}
+            spy={true}
+            offset={-80}
+            activeClass="text-white font-bold"
+            className="cursor-pointer hover:text-white"
+          >
+            Solution
+          </Link>
+          <Link
+            to="features"
+            smooth={true}
+            spy={true}
+            offset={-80}
+            activeClass="text-white font-bold"
+            className="cursor-pointer hover:text-white"
+          >
+            Features
+          </Link>
+          <Link
+            to="tech"
+            smooth={true}
+            spy={true}
+            offset={-80}
+            activeClass="text-white font-bold"
+            className="cursor-pointer hover:text-white"
+          >
+            Tech
+          </Link>
+          <Link
+            to="start"
+            smooth={true}
+            spy={true}
+            offset={-80}
+            activeClass="text-white font-bold"
+            className="cursor-pointer hover:text-white"
+          >
+            Get Started
+          </Link>
         </nav>
       </div>
     </header>
