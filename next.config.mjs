0abb1b365c2e84e13dd293150a317e5ce2cdfa2b
mhandousa <53653174+mhandousa@users.noreply.github.com>
2025-09-18 01:50:14 +0300

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If you later switch to <Image />, keep this or configure domains.
+  // images: { unoptimized: true },
+};
+
+export default nextConfig;
